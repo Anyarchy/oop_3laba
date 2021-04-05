e32799690fa177e52e6533884b209b99fc955741
heroofgotham <annamikhnenko9@gmail.com>
2021-04-05 23:04:52 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_3laba
+{
+    class Students
+    {
+        private int sizeOfTable;
+        private string[,] students;
+        private int nechai;
+
+        public Students()
+        {
+            string[,] temp = new string[0, 3];
+            students = temp;
+            sizeOfTable = 0;
+            nechai = 0;
+        }
+
+
+        public void AddStudent(string Surname, string Name, string FathersName)
+        {
+            sizeOfTable++;
+            string[,] temp = new string[sizeOfTable, 3];
+            for (int i = 0; i < sizeOfTable - 1; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    temp[i, j] = students[i, j];
+                }
+            }
+            temp[sizeOfTable - 1, 0] = Surname;
+            temp[sizeOfTable - 1, 1] = Name;
+            temp[sizeOfTable - 1, 2] = FathersName;
+            students = temp;
+            if (Surname == "Nechai")
+            {
+                nechai++;
+            }
+        }
+
+        public void Outer()
+        {
+            for (int i = 0; i < sizeOfTable; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(students[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public string[] this[string index]
+        {
+            get
+            {
+                string[] column = new string[sizeOfTable];
+                if (Int32.Parse(index) >= 1 || Int32.Parse(index) <= 3)
+                {
+                    for (int i = 0; i < sizeOfTable; i++)
+                    {
+                        column[i] = students[i, Int32.Parse(index) - 1];
+                    }
+                }
+                return column;
+            }
+        }
+
+        public int Nechai
+        {
+            get
+            {
+                return nechai;
+            }
+        }
+    }
+
+}
---
+++ Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP_3laba
+{
+
+    public class Program
+    {
+        public static void Out(string[] str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            Students student = new Students();
+            student.AddStudent("Nechai", "Oleg", "Petrovich");
+            student.AddStudent("Pupkin", "Vasya", "oleksandrovich");
+            student.AddStudent("Kotenko", "Kotya", "Kotyakivna");
+            student.AddStudent("Nechai", "Ivan", "Stepanovich");
+            student.AddStudent("Filimononenko", "Filimon", "Filimonovich");
+            student.AddStudent("My", "disgusting", "Friend");
+            student.AddStudent("Be", "Like", "Beach");
+            student.AddStudent("Nechai", "Piedro", "Horniovich");
+            student.AddStudent("Zubenko", "Michael", "Petrovich");
+            student.AddStudent("Zhmyshenko", "Valery", "Albertovich");
+            student.Outer();
+            Out(student["1"]);
+            Console.WriteLine(student.Nechai);
+        }
+    }
+}
